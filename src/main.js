a25ebecfa422e5459250246633c42a2ebb92e7a6
SomeoneAtLeast <winecooler@yandex.ru>
2020-09-19 17:46:31 +0300

    Информация о фильмах добавляется в разметку из моков + так же добавяется текст кнопок управления + превью открывает подробную информацию

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ import {createShowMoreBtnTemplate} from "./components/Show-More-Btn-Template.js"
 import {createTopRatedTemplate} from "./components/Top-Rated-Template.js";
 import {createFilmDetailsTemplate} from "./components/Film-details.js";
 import {generateFilmsCards} from "./mock/Film-Card.js";
-
+import {managingAMoviesData} from "./mock/Managing-A-movie.js";
+import {createFilmControlsTemplate} from "./components/Film-controls.js";
 
 const render = (container, template, place) => {
     container.insertAdjacentHTML(place, template)
@@ -29,6 +30,8 @@ const filmsListContainer = document.querySelector(`.films-list__container`);
 
 const filmCards = generateFilmsCards();
 
+const ControlsData = managingAMoviesData();
+
 render (filmsListContainer, createFilmCardTemplate(filmCards), `beforeend`)
 
 
@@ -49,24 +52,65 @@ for (let i = 0; i < 2; i++) {
         render (extraFilmsListContainer[i], createFilmCardTemplate(filmCards), `beforeend`)
 };
 
-render (siteMain, createFilmDetailsTemplate(filmCards[0].name), `beforeend`);
+let сlickСaught = false;
 
-// Открытие и закрытие попапа
-const filmDetals = document.querySelector(`.film-details`);
-const filmDetalsEcsBtn = document.querySelector(`.film-details__close-btn`);
-const films = document.querySelectorAll(`.films`);
+const OnCatchAClick = function () {
+  const target = event.target;
+  const filmCard = event.target.closest('.film-card');
+  const filmCardsCollection = document.querySelectorAll(`.film-card`);
 
-films.forEach(function(i) {
-    i.addEventListener(`click`, function () {
-        filmDetals.classList.remove(`popup-close`);
 
-        if (true) {
+  if (!filmCard ||
+      target.classList.contains(`film-card__controls-item--add-to-watchlist`) ||
+      target.classList.contains(`film-card__controls-item--mark-as-watched`) ||
+      target.classList.contains(`film-card__controls-item--favorite`)) {
+        return;
+      };
 
-        };
-      });
+  сlickСaught = true;
+
+  let num = -1;
+
+  filmCardsCollection.forEach(function(i) {
+
+    num++
+
+    if (i === filmCard) {
+
+      // Рендер информации о фильме
+
+      render (siteMain, createFilmDetailsTemplate(filmCards[num].name, filmCards[num].rating, filmCards[num].director,
+      filmCards[num].writers, filmCards[num].actors, filmCards[num].year, filmCards[num].duration, filmCards[num].country,
+      filmCards[num].genre, filmCards[num].poster, filmCards[num].description, filmCards[num].age), `beforeend`);
+      
+     // Рендер кнопок управления
+
+      const formDetailsTopContainer = document.querySelector(`.form-details__top-container`);
+
+      render (formDetailsTopContainer, createFilmControlsTemplate(ControlsData[0], ControlsData[1], ControlsData[2]), `beforeend`);
+    };
+  });
+};
+
+const showAndClosePopup = function () {
+
+  // Открытие
+
+  const filmDetals = document.querySelector(`.film-details`);
+
+  filmDetals.classList.remove(`popup-close`);
+
+  // Закрытие попапа
+
+  const filmDetalsEcsBtn = document.querySelector(`.film-details__close-btn`);
+
+  filmDetalsEcsBtn.addEventListener(`click`, function () {
+    siteMain.removeChild(filmDetals);
+  });
+
+};
+
+filmsListContainer.addEventListener(`click`, function () {
+  OnCatchAClick();
+  showAndClosePopup();
 });
-
-filmDetalsEcsBtn.addEventListener(`click`, function () {
-  filmDetals.classList.add(`popup-close`);
-});
-
